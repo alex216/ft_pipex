e8942c76b57f21b678d6729c20995cceb0274047
yliu <yliu@XLIJFELKNIX.local>
2024-02-18 18:34:39 +0900

    [delete] libft standalone to change to use submodule

--- src/main.c
+++ src/main.c
@@ -6,12 +6,34 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 17:57:53 by yliu              #+#    #+#             */
-/*   Updated: 2024/02/16 16:37:21 by yliu             ###   ########.fr       */
+/*   Updated: 2024/02/18 11:47:51 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "pipex.h"
+#include <string.h>
+
+void	_create_absolute_cmd_name(char *cmd_name)
+{
+	// char *path_adr;
+	// check if environ++ is allowed.
+
+	int	i;
+	char	**m_cmds;
+
+	(void)m_cmds;
+	(void)cmd_name;
+	i = 0;
+	while (environ[i])
+	{
+		if (strncmp(environ[i], "PATH=", 5))
+			break;
+		i++;
+	}
+	ft_printf("%s\n", environ[i]);
+
+}
 
 // since this is not multi-thread process,
 // malloc is safe to use.
@@ -19,6 +41,7 @@ void	_exec_child(char *cmd, int pipefd[])
 {
 	char	**cmdstr;
 
+	_create_absolute_cmd_name(cmd);
 	cmdstr = xft_split(cmd);
 	redirect_fd(pipefd[1], STDOUT_FILENO);
 	close(pipefd[0]);
