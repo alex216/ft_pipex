dc69f78758c97a5f7c2830515cd4aaa1c4204596
yliu <yliu@XLIJFELKNIX.local>
2024-03-04 19:37:30 +0900

    [feat] outfile no write permision exit at 1

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 14:29:58 by yliu              #+#    #+#             */
-/*   Updated: 2024/03/01 22:08:44 by yliu             ###   ########.fr       */
+/*   Updated: 2024/03/04 19:35:52 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 void	exit_errno_msg(const char *errno_msg)
 {
 	ft_dprintf(STDERR_FILENO, "%s\n", errno_msg);
-	exit(42);
+	exit(1);
 }
 
 void	exit_with_perror(const char *errno_msg)
