f2d6ed70bcc19994550f30e37618c7a35cc89227
yliu <yliu@XLIJFELKNIX.local>
2024-05-04 23:20:59 +0900

    [style] use strcmp instead of strncmp

--- src/here_doc/here_doc.c
+++ src/here_doc/here_doc.c
@@ -6,20 +6,15 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 21:50:30 by yliu              #+#    #+#             */
-/*   Updated: 2024/05/04 22:47:12 by yliu             ###   ########.fr       */
+/*   Updated: 2024/05/04 23:17:09 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
-#include "ft_str.h"
 #include "here_doc.h"
 
 int	is_heredoc(const char **argv)
 {
-	int	len;
-
-	len = ft_strlen("here_doc");
-	if (!ft_strncmp(argv[1], "here_doc", len) && argv[1][len] == '\0')
+	if (!ft_strcmp(argv[1], "here_doc"))
 		return (1);
 	else
 		return (0);
