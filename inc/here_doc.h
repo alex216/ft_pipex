f2233eeef81f81d5a80aa0e93ed62d13b70d0f69
yliu <yliu@XLIJFELKNIX.local>
2024-05-05 15:07:38 +0900

    [test] pre-commit

--- inc/here_doc.h
+++ inc/here_doc.h
@@ -6,16 +6,18 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 21:50:55 by yliu              #+#    #+#             */
-/*   Updated: 2024/05/04 23:55:56 by yliu             ###   ########.fr       */
+/*   Updated: 2024/05/05 15:07:27 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HERE_DOC_H
 # define HERE_DOC_H
 
+# include "get_next_line.h"
 # include "libft.h"
+# include "xwrapper.h"
 
 int	is_heredoc(const char **argv);
-int	return_heredoc_fd(const char *limiter);
+int	open_heredoc_fd(const char *limiter);
 
 #endif
